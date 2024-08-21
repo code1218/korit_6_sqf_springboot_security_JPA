14baf61675c33cf1eb46800738fe567444a3f927
code1218 <skjil1218@gmail.com>
2024-08-21 18:01:37 +0900

    Validation AOP 구현

---
+++ SpringSecurity/src/main/java/com/study/SpringSecurity/aspect/TestAspect2.java
@@ -0,0 +1,25 @@
+package com.study.SpringSecurity.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TestAspect {
+
+    @Pointcut("execution(* com.study.SpringSecurity.service.*.aop*(..))")
+    private void pointCut() {}
+
+    @Around("pointCut()")
+    public Object around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+
+        System.out.println("전처리");
+        Object result = proceedingJoinPoint.proceed();  // 핵심기능 호출
+        System.out.println("후처리");
+
+        return result;
+    }
+}
